21b5351e9889db3d743029efc2d35da969248ecb
Alard <alard@example.com>
2011-07-05 12:19:33 +0200

    With WARC, disable --no-clobber, --continue and timestamping.
    See GH-10.

--- trunk/src/main.c
+++ trunk/src/main.c
@@ -1186,6 +1186,31 @@ for details.\n\n"));
            }
     }
 
+  if (opt.warc_filename != 0)
+    {
+      if (opt.noclobber)
+      {
+        fprintf (stderr,
+                 _("WARC output does not work with --no-clobber, "
+                   "--no-clobber will be disabled.\n"));
+        opt.noclobber = false;
+      }
+      if (opt.timestamping)
+      {
+        fprintf (stderr,
+                 _("WARC output does not work with timestamping, "
+                   "timestamping will be disabled.\n"));
+        opt.timestamping = false;
+      }
+      if (opt.always_rest)
+      {
+        fprintf (stderr,
+                 _("WARC output does not work with --continue, "
+                   "--continue will be disabled.\n"));
+        opt.always_rest = false;
+      }
+    }
+
   if (opt.ask_passwd && opt.passwd)
     {
       fprintf (stderr,
